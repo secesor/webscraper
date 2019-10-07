a4c33d5bdff9ab0e6ef6f61450b012d6c393e93f
Sergio Ceron <sd.ceronsoria@um.es>
2019-10-07 18:56:55 +0200

    Initial commit

---
+++ src/test/java/com/secesor/webscraper/WebscraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secesor.webscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebscraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
